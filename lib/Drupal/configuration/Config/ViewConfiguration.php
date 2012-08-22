38c24b016fe1b8fe4d2605545e6639168a76126a
Mariano D'Agostino <dagmar@154086.no-reply.drupal.org>
2012-08-22 16:44:00 -0300

    Issue #1744742 by dagmar: Add required modules to load a view.

--- lib/Drupal/configuration/Config/ViewConfiguration.php
+++ lib/Drupal/configuration/Config/ViewConfiguration.php
@@ -16,4 +16,21 @@ class ViewConfiguration extends CtoolsConfiguration {
 
   // The table where the configurations are storaged.
   static protected $table = 'views_view';
+
+  public function findRequiredModules() {
+    $this->addToModules('views');
+    $view = $this->getData();
+    foreach (views_object_types() as $type => $info) {
+      foreach ($view->display as $display_id => $display) {
+        $view->set_display($display_id);
+        foreach ($view->display_handler->get_handlers($type) as $handler_id => $handler) {
+          if ($type == 'field') {
+            if (!empty($handler->field_info) && !empty($handler->field_info['module'])) {
+              $this->addToModules($handler->field_info['module']);
+            }
+          }
+        }
+      }
+    }
+  }
 }
