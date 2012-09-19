5bd88d2f3a9b5ab51a288be4a013824073bf8ef5
GDrupal <GDrupal@1161294.no-reply.drupal.org>
2012-09-19 16:36:04 -0300

    Issue #1789368: Improve the dependency and required modules logic of Views handler.

--- lib/Drupal/configuration/Config/ViewConfiguration.php
+++ lib/Drupal/configuration/Config/ViewConfiguration.php
@@ -20,8 +20,17 @@ class ViewConfiguration extends CtoolsConfiguration {
   public function findRequiredModules() {
     $this->addToModules('views');
     $view = $this->getData();
-    foreach (views_object_types() as $type => $info) {
+
+    // We get the module that creates the table for the view query.
+    $schema = drupal_get_schema($view->base_table);
+    $this->addToModules($schema['module']);
+
+    foreach (views_object_types () as $type => $info) {
       foreach ($view->display as $display_id => $display) {
+        // Views with a display provided by views_content module.
+        if ($display->display_plugin == 'panel_pane') {
+          $this->addToModules('views_content');
+        }
         $view->set_display($display_id);
         foreach ($view->display_handler->get_handlers($type) as $handler_id => $handler) {
           if ($type == 'field') {
@@ -46,11 +55,32 @@ class ViewConfiguration extends CtoolsConfiguration {
       $config_data = $handler;
 
       foreach ($config_data->conf['display']->content as $object) {
-        list($type, $id, ) = explode('-', $object->subtype);
-        if ($type == 'views') {
-          $view = new ViewConfiguration($id);
-          $view->build();
-          $config->addToDependencies($view);
+        $type = $object->type;
+        switch ($type) {
+          case 'block':
+            list($subtype, $id, ) = explode('-', $object->subtype);
+            switch ($subtype) {
+              // Display block from a view.
+              case 'views':
+                $view = new ViewConfiguration($id);
+                $view->build();
+                $config->addToDependencies($view);
+                break;
+            }
+            break;
+          // A view added directly.
+          case 'views':
+            $view = new ViewConfiguration($object->subtype);
+            $view->build();
+            $config->addToDependencies($view);
+            break;
+          // A view added using the Views content panes module.
+          case 'views_panes':
+            list($subtype, ) = explode('-', $object->subtype);
+            $view = new ViewConfiguration($subtype);
+            $view->build();
+            $config->addToDependencies($view);
+            break;
         }
       }
     }
