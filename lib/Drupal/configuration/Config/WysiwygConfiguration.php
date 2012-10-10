c550f453728ba7e743e36f30816dcad941a0350a
Mariano D'Agostino <dagmar@154086.no-reply.drupal.org>
2012-10-10 12:39:46 -0300

    Issue #1807772 by dagmar: Added Support for Wysiwyg Module.

---
+++ lib/Drupal/configuration/Config/WysiwygConfiguration.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\configuration\Config\WysiwygConfiguration.
+ */
+
+namespace Drupal\configuration\Config;
+
+use Drupal\configuration\Config\Configuration;
+use Drupal\configuration\Utils\ConfigIteratorSettings;
+
+class WysiwygConfiguration extends Configuration {
+
+  static protected $component = 'wysiwyg';
+
+  protected function prepareBuild() {
+    $this->data = wysiwyg_get_profile($this->getIdentifier());
+    return $this;
+  }
+
+  /**
+   * Returns all the identifiers available for this component.
+   */
+  public static function getAllIdentifiers() {
+    $profiles = array();
+
+    $formats = filter_formats();
+
+    foreach (array_keys(wysiwyg_profile_load_all()) as $format) {
+      // Text format may vanish without deleting the wysiwyg profile.
+      if (isset($formats[$format])) {
+        $profiles[] = $format;
+      }
+    }
+    return $profiles;
+  }
+
+  public static function alterDependencies(Configuration $config, &$stack) {
+    if ($config->getComponent() == 'text_format') {
+      $formats = filter_formats();
+      foreach (array_keys(wysiwyg_profile_load_all()) as $format) {
+        // Text format may vanish without deleting the wysiwyg profile.
+        if (isset($formats[$format]) && $format == $config->getIdentifier()) {
+          $identifier = $format;
+          if (empty($stack['wysiwyg.' . $identifier])) {
+            $wysiwig_profile = new WysiwygConfiguration($identifier);
+            $wysiwig_profile->build();
+
+            $config->addToOptionalConfigurations($wysiwig_profile);
+
+            $wysiwig_profile->addToDependencies($config);
+            $stack['wysiwyg.' . $identifier] = TRUE;
+          }
+        }
+      }
+    }
+  }
+
+  public function findDependencies() {
+    $format = $this->getIdentifier();
+
+    $formats = filter_formats();
+    if (isset($formats[$format])) {
+      $class = Configuration::getConfigurationHandler('text_format');
+      $filter_format = new $class($format);
+      $this->addToDependencies($filter_format);
+    }
+
+    parent::findDependencies();
+  }
+
+  public function findRequiredModules() {
+    $this->addToModules('wysiwyg');
+    // @todo figure out if there is a way to add modules that provides plugins
+    // for this wysiwyg
+  }
+
+  public function saveToActiveStore(ConfigIteratorSettings &$settings) {
+    $profile = $this->getData();
+
+    // For profiles that doens't have editors assigned, provide a default
+    // object to avoid sql exceptions.
+    if (empty($profile)) {
+      $profile = new \StdClass();
+      $profile->editor = '';
+      $profile->format = $this->getIdentifier();
+      $profile->settings = array();
+    }
+
+    db_merge('wysiwyg')
+      ->key(array('format' => $profile->format))
+      ->fields(array(
+        'format' => $profile->format,
+        'editor' => $profile->editor,
+        'settings' => serialize($profile->settings),
+      ))
+      ->execute();
+    wysiwyg_profile_cache_clear();
+
+    $settings->addInfo('imported', $this->getUniqueId());
+  }
+}
